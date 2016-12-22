ba0fcb68987396055ae2098c7460abad8aa007f1
thopaw <thomas.pawlitzki@gmail.com>
2016-12-22 14:23:21 +0100

    added examples for deconstruction

--- examples/src/examples/core.clj
+++ examples/src/examples/core.clj
@@ -19,13 +19,14 @@
 (t-c-f (fn [] (println "hello")))
 (t-c-f (fn [] (throw  (Exception. "buh"))))
 
-
+; creates a new list
 (list 1 2 3 4)
 
+; creates a list and evals it
 (+ 1 2 3 4)
-
 (eval (list + 1 2 3 4))
 
+; iterates over a sequence
 (defn print-seq [s]
   (if (seq s)
     (do
@@ -36,13 +37,30 @@
 )
 
 (print-seq [1 2 3 4])
+(print-seq (list 1 2 3 4))
 
-(defn print-name [dek-full-name]
+
+; de-construct a sequence
+(defn print-name-let [dek-full-name]
   (let [ [f m l] dek-full-name ]
   (println "first: " f)
   (println "middle: " m)
   (println "last: " l)))
 
-(print-name ["a" "b" "c"])
+(defn print-name [ [f m l] ]
+  (println "first: " f)
+  (println "middle: " m)
+  (println "last: " l))
 
+(print-name ["a" "b" "c"])
 (print-name (list "a" "b" "c"))
+
+; de-constructs with head and tail
+(let [ [x & r] '(1 2 3 4)]  {:x x, :r r})
+
+; de-constructs a map
+(def dek-map {:first "Donald", :middle "J", :last "Simpson"})
+(let [ {first :first, middle :middle, last :last} dek-map]
+  (println "first: " first)
+  (println "middle: " middle)
+  (println "last: " last))
