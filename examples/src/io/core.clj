11ae26f10d23f597152e6f749d1bc753fc2ecc64
thopaw <thomas.pawlitzki@gmail.com>
2016-12-23 11:36:59 +0100

    io examples

--- examples/src/io/core.clj
+++ examples/src/io/core.clj
@@ -1,4 +1,15 @@
-(ns io.core)
+(ns io.core (:require [clojure.java.io :as cio]))
 
 ; reads file in string
 (slurp "README.md")
+;(slurp "doc/intro.md")
+
+(with-open
+  [xin (cio/reader "project.clj")]
+  (loop []
+    (let [x (.read xin)]
+    (if (pos? x)
+      (do
+        (print (char x))
+        (recur)
+      )))))
