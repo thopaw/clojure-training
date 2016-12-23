5c68f2855e47780bdcef410caccf3b6538e1588e
thopaw <thomas.pawlitzki@gmail.com>
2016-12-23 11:47:48 +0100

    macro examples

--- examples/macros/core.clj
+++ examples/macros/core.clj
@@ -1,3 +1,13 @@
 (ns macros.core)
 
 ; define macros with defmacro
+(defmacro infix [x]
+  (list (second x) (first x) (last x)))
+
+(infix (3 + 4)) ; switches argument positions
+
+; macro with de-constriction pattern
+(defmacro infix-2 [ [op1 op2 op3] ]
+  (list op2 op1 op3))
+
+(infix-2 (3 + 4))
